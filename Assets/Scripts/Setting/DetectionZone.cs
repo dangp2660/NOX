b47dfe3557938fc8d6ee230b03d5268294d22937
dangp2660 <154449675+dangp2660@users.noreply.github.com>
2025-04-20 18:34:56 +0700

    stop when enemy attack (20/4/2025)

--- Assets/Scripts/Setting/DetectionZone.cs
+++ Assets/Scripts/Setting/DetectionZone.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class DetectionZone : MonoBehaviour
 {
     public List<Collider2D> detectedCollider = new List<Collider2D>();
+    private Dictionary<Collider2D, Coroutine> removeTimers = new Dictionary<Collider2D, Coroutine>(); // To store coroutines for each collider
+
     private void Awake()
     {
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-         detectedCollider.Add(collision);  
+        detectedCollider.Add(collision);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        detectedCollider.Remove(collision);
+        // If there is already a coroutine running for this collider, stop it.
+        if (removeTimers.ContainsKey(collision))
+        {
+            StopCoroutine(removeTimers[collision]);
+            removeTimers.Remove(collision);
+        }
+
+        // Start a new coroutine to remove it after 1.2 seconds
+        Coroutine removeCoroutine = StartCoroutine(RemoveAfterDelay(collision));
+        removeTimers.Add(collision, removeCoroutine);
     }
     private void Update()
     {
@@ -25,6 +36,17 @@ public class DetectionZone : MonoBehaviour
             {
                 detectedCollider.RemoveAt(i);
             }
+        }
+    }
+
+    private IEnumerator RemoveAfterDelay(Collider2D collider)
+    {
+        yield return new WaitForSeconds(0.7f);
+
+        // Only remove it if it has not already been removed
+        if (detectedCollider.Contains(collider))
+        {
+            detectedCollider.Remove(collider);
         }
     }
 }
