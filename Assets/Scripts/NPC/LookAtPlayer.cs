c037ace5dcb9bd3d1be3391bd32ac615cf4e637b
dangp2660 <154449675+dangp2660@users.noreply.github.com>
2025-05-02 16:57:49 +0700

    5/2/2025
    Dialogue NPC

---
+++ Assets/Scripts/NPC/LookAtPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform player; // Tham chiếu đến Player
+
+    private float initialScaleX;
+
+    private void Start()
+    {
+        // Lưu lại hướng ban đầu để lật đúng
+        initialScaleX = transform.localScale.x;
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+
+        float direction = player.position.x - transform.position.x;
+
+        if (Mathf.Abs(direction) > 0.01f)
+        {
+            // Nếu Player ở bên phải -> scale.x dương, bên trái -> âm
+            float newScaleX = direction > 0 ? Mathf.Abs(initialScaleX) : -Mathf.Abs(initialScaleX);
+            transform.localScale = new Vector3(newScaleX, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NPCChat.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NPCChat : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index;
+    public float worldSpeed;
+    public bool closePlayer;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (closePlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!dialoguePanel.activeInHierarchy)
+            {
+                dialoguePanel.SetActive(true);
+                index = 0;
+                dialogueText.text = "";
+                StartCoroutine(Typing());
+            }
+            else if (dialogueText.text == dialogue[index])
+            {
+                nextLine(); 
+            }
+            else
+            {
+                // Nếu đang gõ dở thì hiện toàn bộ câu ngay lập tức
+                StopAllCoroutines();
+                dialogueText.text = dialogue[index];
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            closePlayer = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            closePlayer = false;
+    }
+  
+
+    public void nextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(worldSpeed);
+        }
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+}
