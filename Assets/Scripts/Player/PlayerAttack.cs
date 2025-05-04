844344b3f2cef309914f8d8c447b514900cfa130
dangp2660 <154449675+dangp2660@users.noreply.github.com>
2025-05-04 14:33:15 +0700

    5/4/2025
    optimize use spell and add changing

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,3 +1,5 @@
+﻿using System.Collections;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,15 +7,18 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
     private Animator animator;
-
+    [SerializeField] private GameObject changingSpell;
+    [SerializeField] private Transform changingPosition;
+    private GameObject activeSpell;
+    [SerializeField] private float coolDown = 2f;
+    private bool canUseSkill = true;
 
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
-
     }
+    
 
     public void HandleAttackInput(InputAction.CallbackContext context)
     {
@@ -27,8 +32,37 @@ public class PlayerAttack : MonoBehaviour
     public void HandleMagic(InputAction.CallbackContext context)
     {
         if (context.started)
+            Debug.Log("Nhấn phím magic");
+
+        if (context.started && canUseSkill)
         {
+            Debug.Log("Dùng spell");
+
+            canUseSkill = false;
+
+            if (changingSpell != null && changingPosition != null)
+            {
+                activeSpell = Instantiate(changingSpell, changingPosition.position, Quaternion.identity);
+                Destroy(activeSpell, 2f);
+            }
+
             animator.SetTrigger(AnimationStringList.MagicAttack);
+            StartCoroutine(Delay(coolDown));
         }
     }
+
+    IEnumerator Delay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+       
+        canUseSkill = true;
+        Debug.Log(canUseSkill);
+    }
+
+    public void OnDestroy()
+    {
+        Destroy(activeSpell, 0.5f);
+    }
+    
+
 }
